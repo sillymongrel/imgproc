3ea3becdbc4c645670dee282b41295f4622174bd
lyrebirdsw <growlercab@gmail.com>
2014-07-03 14:31:52 +1000

    fix image read

--- histeq.cc
+++ histeq.cc
@@ -4,39 +4,19 @@
 #include <stdint.h>
 
 
-const int WIDTH = 240;
-const int HEIGHT = 320;
+int WIDTH = 240;
+int HEIGHT = 320;
 
 
 
-uint8_t* readPGM(const char* fname)
-{
-    FILE* fptr = fopen(fname, "rb");
-    if(!fptr) {
-        printf("failed to read file '%s'\n", fname);
-        return NULL;
-    }
-    fseek(fptr, 0, SEEK_END);
-    int flen = ftell(fptr);
-    fseek(fptr, 0xF, SEEK_SET);// Skip the image header.
-
-    uint8_t* ret = (uint8_t*)malloc(flen+1*sizeof(uint8_t));
-    fread(ret, flen, 1, fptr); 
-    fclose(fptr);
-    return ret;
-}
-
-void writePGM(const char* fname, const uint8_t* img, const int w, const int h)
-{
-    FILE* fptr = fopen(fname, "wb");
-    const char outdat[] = "P5 240 320 255 ";
-    fwrite(outdat, sizeof(char), sizeof(outdat), fptr);
-    fwrite(img, sizeof(uint8_t), w*h, fptr);
-    fclose(fptr);
-}
 /// Compute and return the histogram of a geryscale image. 
+/// @param img      Input image pixels
+/// @param w        Width of the input image
+/// @param h        Height of the input image
+/// @return Histogram of the image.
 float* hist(const uint8_t* img, const int w, const int h) 
 {
+    float npix = w*h;
     float* ret = (float*)malloc(256 * sizeof(float));
     memset(ret, 0, 256*sizeof(float));
     for(int y = 0; y < h; ++y) {
@@ -46,28 +26,36 @@ float* hist(const uint8_t* img, const int w, const int h)
         }
     }
     for(int i = 0; i < 256; ++i) {
-        ret[i] /= (float)(w*h);
+        ret[i] /= npix;
     }
     return ret;
 }
 
-// Compute the histogram equalised image. Histogram equalisation is computed
-// using the following formula:
-//
-// g_i = (M-1)/N SUM^i_{j=0} n_i
-// 
-// where:
-// M = 256 (total number of greylevels)
-// N = total nunber of pixels in the image
-// n_i is the number of pixels at greylevel i.
-// 
-// The algorithm uniformly redistributes image greylevels so the probability
-// of each greylevel is approximately the same. 
+/// Compute the histogram equalised image. Histogram equalisation is computed
+/// using the following formula:
+///
+/// g_i = (M-1)/N SUM^i_{j=0} n_i
+/// 
+/// where:
+/// M = 256 (total number of greylevels)
+/// N = total nunber of pixels in the image
+/// n_i is the number of pixels at greylevel i.
+/// 
+/// Using the CDF, the algorithm uniformly redistributes 
+/// image greylevels so the probability of each greylevel 
+/// is approximately the same. 
+///
+/// @param result       Output image buffer
+/// @param img          Input image pixels
+/// @param w            Width of the input image
+/// @param h            Height of the input image
 void histeq(uint8_t* result, const uint8_t* img, const int w, const int h)
 {
     float* imgHist=hist(img, w, h);
     uint32_t* histEq = (uint32_t*)malloc(256*sizeof(uint32_t)); 
     memset(histEq, 0, 256*sizeof(uint32_t));
+
+    // Compute CDF and rescale to 0-255 for each new greylevel.
     for(int i = 0; i < 256; ++i) {
         float sum = 0.0f;
         for(int j = 0; j <= i; ++j) {
@@ -76,6 +64,8 @@ void histeq(uint8_t* result, const uint8_t* img, const int w, const int h)
         histEq[i] = (int)(sum * 255.0f + 0.5f); // round
     }
     free(imgHist);
+
+    // Apply the new greylevels to the output image
     for(int y = 0; y < h; ++y) {
         const uint8_t* scan = &img[y*w];
         uint8_t* resScan = &result[y*w];
@@ -87,6 +77,44 @@ void histeq(uint8_t* result, const uint8_t* img, const int w, const int h)
 }
 
 
+uint8_t* readPGM(const char* fname)
+{
+    FILE* fptr = fopen(fname, "r");
+    char dummy[256];
+    if(!fptr) {
+        printf("failed to read file '%s'\n", fname);
+        return NULL;
+    }
+    // Skip the image header.
+    fgets(dummy, 255, fptr);
+    if(dummy[0] != 'P' && dummy[1] != '5') {
+        printf("image not recognised\n");
+        exit(1);
+    }
+    fgets(dummy, 256, fptr);
+    if(dummy[0] == '#') {
+        fgets(dummy, 256, fptr);
+    }
+
+    sscanf(dummy, "%d %d", &WIDTH, &HEIGHT);
+    fgets(dummy, 256, fptr);
+
+    printf("Image:%s\nWidth:%d\nHeight:%d\n", fname, WIDTH, HEIGHT);
+
+    uint8_t* ret = (uint8_t*)malloc(WIDTH*HEIGHT*sizeof(uint8_t));
+    fread(ret, WIDTH*HEIGHT, 1, fptr); 
+    fclose(fptr);
+    return ret;
+}
+
+void writePGM(const char* fname, const uint8_t* img, const int w, const int h)
+{
+    FILE* fptr = fopen(fname, "wb");
+    const char outdat[] = "P5\n#Equalised image\n240 320\n255\n";
+    fwrite(outdat, sizeof(char), sizeof(outdat), fptr);
+    fwrite(img, sizeof(uint8_t), w*h, fptr);
+    fclose(fptr);
+}
 
 
 int main(int argc, char** argv)
