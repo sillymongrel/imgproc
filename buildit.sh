7fa6bb24eace05440409a92f070d02e5d34e90f7
lyrebirdsw <growlercab@gmail.com>
2014-07-28 15:43:05 +1000

    rectangular crop

--- buildit.sh
+++ buildit.sh
@@ -1,1 +1,18 @@
-g++ -g histeq.cc
+CC=g++
+CFLAGS=-Wall -g
+
+image.o:
+    $(CC) $(CFLAGS) -c image.c
+
+histeq: image.o
+    $(CC) $(CFLAGS) image.o histeq.c -o histeq
+
+roi: image.o
+    $(CC) $(CFLAGS) image.o roi.c -o roi
+
+all: histeq roi
+
+clean:
+    rm -fr *.o histeq roi
+
+    
